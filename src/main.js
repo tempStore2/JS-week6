3990295d89ef782b3e719824a9d51f437591da41
XinUndfined <z26319491z@yahoo.com.tw>
2020-07-26 21:53:44 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+Vue.component('Loading', Loading)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
